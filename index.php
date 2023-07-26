f7c1803d968144bb0da6cfe0764a415768a0d7e5
KheuhyBarral <130567554+KheuhyBarral@users.noreply.github.com>
2023-07-26 19:16:09 -0300

    Base das paginas: index e a de questões

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Web dev</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+      <h1>Desenvolvimento Web</h1>
+    </header>
+    <main>
+      <div class="questoes">
+        <h2>Trabalho: Questões</h2>
+        <ul>
+          <li><a href="./q1.php">Questão 1</a></li>
+          <li><a href="./q2.php">Questão 2</a></li>
+          <li><a href="./q3.php">Questão 3</a></li>
+          <li><a href="./q4.php">Questão 4</a></li>
+        </ul>
+      </div>
+      
+    </main>
+    <footer>
+      <p>Edson Júnior e Kheuhy - &copy; 2023</p>
+    </footer>
+  </body>
+</html>
---
+++ q1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Questão 1 - Web dev</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+      <h1>Desenvolvimento Web</h1>
+    </header>
+    <main>
+      <div class="questoes">
+        <h2>Trabalho: Questão 1</h2>
+        <a href="../index.php"><button>Voltar</button></a>
+      </div>
+    </main>
+    <footer>
+      <p>Edson Júnior e Kheuhy - &copy; 2023</p>
+    </footer>
+  </body>
+</html>
