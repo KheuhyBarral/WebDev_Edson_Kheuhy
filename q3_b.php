0258859ffe1246d9dcc8fdb628338212b0c96122
KheuhyBarral <130567554+KheuhyBarral@users.noreply.github.com>
2023-07-27 11:35:09 -0300

    questão 3 finalizada

--- q3_b.php
+++ q3_b.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Questão 3 - Web dev</title>
     <link rel="stylesheet" href="style.css" />
+    <?php
+    $r1 = isset($_GET["r1"]) ? $_GET["r1"] : 0;
+    ?>
 </head>
 
 <body>
@@ -17,26 +20,27 @@
             <h2>Trabalho: Questão 3</h2>
 
             <div class="pergunta">
-                <h3>Q1: Quem foi o fundador do PHP?</h2>
+                <h3>Q2: Qual é o objetivo do PHP?</h2>
                     <form id="questao" action="q3.php" method="get">
                         <div class="resposta">
                             <ol type="A">
-                                <a href="q3_b.php?r1=1">
-                                    <li>Guido van Rossum</li>
+                                <a href="q3_c.php?r1=<?php echo $r1 ?>&r2=1">
+                                    <li>Criar páginas web dinâmicas</li>
                                 </a>
-                                <a id="resp" href="q3_b.php?r1=2">
-                                    <li>Rasmus Lerdorf</li>
+                                <a href="q3_c.php?r1=<?php echo $r1 ?>&r2=2">
+                                    <li>Processar dados do lado do servidor</li>
                                 </a>
-                                <a href="q3_b.php?r1=3">
-                                    <li>Bjarne Stroustrup</li>
+                                <a href="q3_c.php?r1=<?php echo $r1 ?>&r2=3">
+                                    <li>Criar aplicações web</li>
                                 </a>
-                                <a href="q3_b.php?r1=4">
-                                    <li>Linus Torvalds</li>
+                                <a id="resp" href="q3_c.php?r1=<?php echo $r1 ?>&r2=4">
+                                    <li>Todas as anteriores</li>
                                 </a>
-                                <a href="q3_b.php?r1=5">
-                                    <li>Dennis Ritchie</li>
-                                </a>
+
+
                             </ol>
+                            <a href="../q3.php"><button>Cancelar</button></a>
+
                     </form>
             </div>
         </div>
---
+++ q3_c.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Questão 3 - Web dev</title>
+    <link rel="stylesheet" href="style.css" />
+    <?php
+    $r1 = isset($_GET["r1"]) ? $_GET["r1"] : 0;
+    $r2 = isset($_GET["r2"]) ? $_GET["r2"] : 0;
+    ?>
+</head>
+
+<body>
+    <header>
+        <h1>Desenvolvimento Web</h1>
+    </header>
+    <main>
+        <div class="questoes">
+            <h2>Trabalho: Questão 3</h2>
+
+            <div class="pergunta">
+                <h3>Q3: Quem são os professores de programação PHP e HTML do IFNMG-MOC?</h2>
+                    <form id="questao" action="q3.php" method="get">
+                        <div class="resposta">
+                            <ol type="A">
+                                <a href="q3_d.php?r1=<?php echo $r1 ?>&r2=<?php echo $r2 ?>&r3=1">
+                                    <li>Edson Júnior</li>
+                                </a>
+                                <a id="resp" href="q3_d.php?r1=<?php echo $r1 ?>&r2=<?php echo $r2 ?>&r3=2">
+                                    <li>Luis Antônio Guisso</li>
+                                </a>
+                                <a href="q3_d.php?r1=<?php echo $r1 ?>&r2=<?php echo $r2 ?>&r3=3">
+                                    <li>Kheuhy Barral</li>
+                                </a>
+                                <a href="q3_d.php?r1=<?php echo $r1 ?>&r2=<?php echo $r2 ?>&r3=4">
+                                    <li>Mario Sergio</li>
+                                </a>
+                                <a href="q3_d.php?r1=<?php echo $r1 ?>&r2=<?php echo $r2 ?>&r3=5">
+                                    <li>Maria Alice Leite</li>
+                                </a>
+
+
+                            </ol>
+                            <a href="../q3.php"><button>Cancelar</button></a>
+
+                    </form>
+            </div>
+        </div>
+
+
+        <a href="../index.php"><button>Voltar</button></a>
+        </div>
+    </main>
+    <footer>
+        <p>Edson Júnior e Kheuhy - &copy; 2023</p>
+    </footer>
+</body>
+
+</html>
