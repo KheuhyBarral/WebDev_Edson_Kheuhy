1f56d86744f6cc74eae361b030c505b374240e2c
KheuhyBarral <130567554+KheuhyBarral@users.noreply.github.com>
2023-07-28 09:15:11 -0300

    questao 4 sem css

--- q4.php
+++ q4.php
@@ -7,8 +7,10 @@
     <title>Questão 4 - Web dev</title>
     <link rel="stylesheet" href="style.css" />
     <?php
-    $qnt = isset($_GET["qnt"]) ? $_GET["qnt"] : 1;
-    $selec = isset($_GET["select"]) ? $_GET["select"] : "none";
+    $aporte_inical = (isset($_GET["aporte_inicial"]) ? $_GET["aporte_inicial"] : 500);
+    $meses = (isset($_GET["meses"]) ? $_GET["meses"] : 12);
+    $rendimento = (isset($_GET["rendimento"]) ? $_GET["rendimento"] : 0.7);
+    $aporte_mensal = (isset($_GET["aporte_mensal"]) ? $_GET["aporte_mensal"] : 350);
     ?>
 </head>
 
@@ -19,13 +21,67 @@
     <main>
         <div class="questoes">
             <h2>Trabalho: Questão 4</h2>
+            <fieldset>
+                <legend>Parâmetros</legend>
+                <form action="q4.php" method="get">
+
+                    <p>Aporte inicial(R$): <input type="number" min="0" max="99999999.99" name="aporte_inicial"
+                            value="<?php echo $aporte_inical ?>" style="width: 100px">[até R$
+                        999.999,99]</p>
+                    <p>Período(meses): <input type="number" min="1" max="480" name="meses" style="width: 100px"
+                            value="<?php echo $meses ?>">[1 até 480]
+                    </p>
+                    <p>Rendimento mensal(%): <input type="number" max="20" name="rendimento" style="width: 50px"
+                            step="0.1" value="<?php echo $rendimento ?>">[até 20%]
+                    </p>
+                    <p>Aporte mensal(R$): <input type="number" min="0" max="99999999.99" name="aporte_mensal"
+                            value="<?php echo $aporte_mensal ?>" style="width: 100px">[até R$
+                        999.999,99]</p>
+                    <a href="../index.php"><button type="submit">Processar</button></a>
+                </form>
+
+            </fieldset>
+
+            <fieldset>
+                <legend>Resultado</legend>
+                <div class="tabela">
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Mês</th>
+                                <th>Valor Inicial (R$)</th>
+                                <th>Aporte (R$)</th>
+                                <th>Rendimento (R$)</th>
+                                <th>Total (R$)</th>
+                            </tr>
+                        <tbody>
+                            <?php
+                            for ($i = 1; $i <= $meses; $i++) {
+                                echo "<tr>";
+                                echo "<td>" . $i . "</td>";
+                                echo "<td>" . number_format($aporte_inical, 2, ",", ".") . "</td>";
+                                echo "<td>" . ($i > 1 ? number_format($aporte_mensal, 2, ",", ".") : "---") . "</td>";
+                                echo "<td>" . number_format($aporte_inical * $rendimento / 100, 2, ",", ".") . "</td>";
+                                $aporte_inical = $aporte_inical * (1 + ($rendimento / 100));
+                                echo "<td>" . number_format($aporte_inical, 2, ",", ".") . "</td>";
+                                $aporte_inical += $aporte_mensal;
+                                echo "</tr>";
+                            }
+
+                            ?>
+                        </tbody>
+                        </thead>
+                    </table>
+                </div>
+            </fieldset>
 
 
             <a href="../index.php"><button>Voltar</button></a>
         </div>
     </main>
     <footer>
-        <p>Edson Júnior e Kheuhy - &copy; 2023</p>
+        <p>Edson Júnior e Kheuhy Barral - &copy; 2023</p>
     </footer>
 </body>
 
